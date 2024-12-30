988bfcf625a6935c75bd6c438c3097827f41c14e
Chris Alvis <alvisleet@gmail.com>
2024-12-29 21:39:42 -0500

    update on interval

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { Container, Grid, Paper, Typography } from "@mui/material";
+import React, {useEffect, useMemo, useState} from "react";
+import {Container, Grid, Paper, Typography} from "@mui/material";
 import CombinedHashRateChart from "./CombinedHashRateChart.tsx";
 import SystemHealth from "./SystemHealth.tsx";
-import { fetchMinerData, TimeSeriesData, fetchMeowcoinData } from "../utils/utils.tsx";
+import {fetchMeowcoinData, fetchMinerData, TimeSeriesData} from "../utils/utils.tsx";
 
 const Index: React.FC = () => {
     // Kaspa-related state
@@ -41,54 +41,60 @@ const Index: React.FC = () => {
     const [meowcoinData, setMeowcoinData] = useState(null);
     useEffect(() => {
         const fetchData = async () => {
-            const data = await fetchMeowcoinData();
-            setMeowcoinData(data);
+            await fetchMeowcoinData().then(data => {
+                setMeowcoinData(data);
+            });
         };
-        fetchData();
+        const intervalId = setInterval(fetchData, 30000);
+        fetchData(); // Initial data fetch
+        return () => clearInterval(intervalId);
     }, []);
 
     const memoedMeowcoinData = useMemo(() => meowcoinData, [meowcoinData]);
 
     return (
-        <Container sx={{ padding: 3, backgroundColor: "#121212", color: "#ffffff" }}>
+        <Container sx={{padding: 3, backgroundColor: "#121212", color: "#ffffff"}}>
             <Grid container spacing={3}>
 
                 {/* Kaspa Section */}
-                <Grid item xs={12} md={8} sx={{ display: { xs: "none", md: "grid" } }}>
-                    <Paper sx={{ padding: 3, backgroundColor: "#1E1E1E", borderRadius: 2 }}>
-                        <Typography variant="h5" gutterBottom sx={{ fontWeight: "bold" }}>
+                <Grid item xs={12} md={8} sx={{display: {xs: "none", md: "grid"}}}>
+                    <Paper sx={{padding: 3, backgroundColor: "#1E1E1E", borderRadius: 2}}>
+                        <Typography variant="h5" gutterBottom sx={{fontWeight: "bold"}}>
                             Combined Miner Hash Rates (Kaspa)
                         </Typography>
-                        <CombinedHashRateChart />
+                        <CombinedHashRateChart/>
                     </Paper>
                 </Grid>
                 <Grid item xs={12} md={4}>
-                    <Grid item xs={12} md={4} sx={{ display: { xs: "grid", md: "none" } }}>
-                        <Paper sx={{ padding: 3, backgroundColor: "#1E1E1E", marginBottom: 2, borderRadius: 2 }} key={"combined"}>
-                            <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                    <Grid item xs={12} md={4} sx={{display: {xs: "grid", md: "none"}}}>
+                        <Paper sx={{padding: 3, backgroundColor: "#1E1E1E", marginBottom: 2, borderRadius: 2}}
+                               key={"combined"}>
+                            <Typography variant="h5" sx={{fontWeight: "bold"}}>
                                 Combined
                             </Typography>
                             <Typography variant="h6">{combinedHashRate}</Typography>
                         </Paper>
                     </Grid>
                     {memoedMiners.map((minerId) => (
-                        <Paper sx={{ padding: 3, backgroundColor: "#1E1E1E", marginBottom: 2, borderRadius: 2 }} key={minerId}>
-                            <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                        <Paper sx={{padding: 3, backgroundColor: "#1E1E1E", marginBottom: 2, borderRadius: 2}}
+                               key={minerId}>
+                            <Typography variant="h5" sx={{fontWeight: "bold"}}>
                                 {minerId.toUpperCase()}
                             </Typography>
-                            <SystemHealth minerId={minerId} />
+                            <SystemHealth minerId={minerId}/>
                         </Paper>
                     ))}
                     {memoedMeowcoinData && (
-                        <Paper sx={{ padding: 3, backgroundColor: "#1E1E1E", marginBottom: 2, borderRadius: 2 }}>
-                            <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                        <Paper sx={{padding: 3, backgroundColor: "#1E1E1E", marginBottom: 2, borderRadius: 2}}>
+                            <Typography variant="h5" sx={{fontWeight: "bold"}}>
                                 Meowcoin
                             </Typography>
                             <Typography variant="h6">
                                 Workers Online: {memoedMeowcoinData.workersOnline}
                             </Typography>
                             <Typography variant="h6">
-                                Hash Rate: {memoedMeowcoinData.modeStats.pplns.default.dayHashrate.toFixed(2) / 1000000} MH/s
+                                Hash
+                                Rate: {memoedMeowcoinData.modeStats.pplns.default.dayHashrate.toFixed(2) / 1000000} MH/s
                             </Typography>
                             <Typography variant="h6">
                                 Monthly Profit: {memoedMeowcoinData.stats.income.income_Month.toFixed(2)} MEWC
